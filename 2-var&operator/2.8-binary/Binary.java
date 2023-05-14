52f13fa90b44c92103a80d6e09de452cafa9b71f
toBeTheLight <yl1991web@126.com>
2023-05-15 00:51:29 +0800

    feat: 进制和算术运算

---
+++ 2-var&operator/2.8-binary/Binary.java
@@ -0,0 +1,14 @@
+class Binary {
+  public static void main (String[] args) {
+    // 10进制
+    int num1 = 103;
+    // 2进制
+    int num2 = 0b10;
+    // 8进制
+    int num3 = 010;
+    // 16进制
+    int num4 = 0x10a;
+
+    System.out.printf("%d,%d,%d,%d", num1, num2, num3, num4);
+  }
+}
---
+++ 2-var&operator/2.9-arithmetic/Arithmetic.java
@@ -0,0 +1,23 @@
+public class Arithmetic {
+  public static void main (String[] args) {
+    int m1 = 12;
+    int m2 = 5;
+    double m3 = 5.0;
+    double m4 = m1 / m2; // 2.0，int 运算还是 int
+    double m5 = m1 / m3; // 2.4
+    double m6 = m1 / m2 * m2; // 10.0
+
+    int a1 = 1;
+    int a2 = 1;
+    System.out.println(a1++ + 1); // 2
+    System.out.println(++a2 + 1); // 3
+
+    short s1 = 10;
+    //s1 = s1 + 1; // 报错，short 与 int 的运算，需要强制类型转换
+    s1 += 1; // 不会报错，不涉及与 int 的运算，只是在 short 的字节上加1
+    s1++; // 不会报错，同上
+
+    byte b1 = 127; 
+    System.out.println(++b1); // -128，自增不会改变类型
+  }
+}
